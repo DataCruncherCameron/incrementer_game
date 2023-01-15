3c7b3d334069585e5790cab7737ccc99094612e0
cam <cameron.mclaren@accenture.com>
2023-01-15 20:09:56 +1100

    Added bulk buy for skill

--- game.js
+++ game.js
@@ -3,6 +3,8 @@
     const number = document.getElementById("wood_number");
     const skill_button = document.getElementById("skill-increment-button")
     const skill_number = document.getElementById("skill_number")
+    const skill_bulk = document.getElementById("bulkbuy-skill")
+    var skill_milestone = true
     function generateRandomNumber(score, skill) {
         let randomNumber = Math.ceil(Math.random() * skill) + score;
         return randomNumber;
@@ -24,7 +26,7 @@
     });
 
     skill_button.addEventListener("click", () => {
-        button.disabled = true; // Disable the button
+        skill_button.disabled = true; // Disable the button
         if (count > 30)
         {   
             count = count - 30
@@ -35,7 +37,39 @@
         {
             console.log("Not enough wood")
         }
+
+
+        if (skill_milestone && (skill > 15))
+        {
+            skill_milestone=false
+            alert("Skill bulk buy enabled")
+            skill_bulk.innerHTML ="<p>Click the button bulk buy wood skill:</p> \
+            <button id='skill-bulk-button'>Bulk increase skill</button>"
+
+            const skill_bulk_button = document.getElementById('skill-bulk-button')
+
+            skill_bulk_button.addEventListener("click", () => {
+                button.disabled = true; // Disable the button
+                
+                while(count > 30)
+                {
+                    count = count - 30;
+                    skill++;
+                }
+                skill_number.innerHTML = `Skill ${skill}`;
+                number.innerHTML = `Wood ${count}`;
+
+                setTimeout(() => {
+                    button.disabled = false; // Re-enable the button after 1 second
+                }, 1);
+            } )  ;
+        }
+
+
+
+
+
         setTimeout(() => {
-            button.disabled = false; // Re-enable the button after 1 second
+            skill_button.disabled = false; // Re-enable the button after 1 second
         }, 1);
     });
