9f63d5ce3be481f4da1c444da08889ee972e2870
cam <cameron.mclaren@accenture.com>
2023-01-26 15:36:23 +1100

    Serious refactoring

--- game.js
+++ game.js
@@ -1,62 +1,103 @@
-      // Get the button and the number element
-    const button = document.getElementById("increment-button");
-    const number = document.getElementById("wood_number");
-    const skill_button = document.getElementById("skill-increment-button")
-    const skill_number = document.getElementById("skill_number")
-    const skill_bulk = document.getElementById("bulkbuy-skill")
-    var skill_milestone = true
-    function generateRandomNumber(score, skill) {
-        let randomNumber = Math.ceil(Math.random() * skill) + score;
+    // Paperwork related elements
+    const paperwork_completing_button = document.getElementById("increment-button");
+    const paperwork_value_p = document.getElementById("paperwork_number");
+    
+    // Hire request related elements 
+    const hire_request_button = document.getElementById("hire_request-increment-button")
+    const hire_request_number = document.getElementById("hire_request_number")
+    const hire_request_bulk = document.getElementById("bulkbuy-hire_request")
+    
+    // Milestone Variables 
+    var paperwork_milestone = 15;
+    var old_paperwork_milestone = 0;
+
+    // Promotion Related Elements 
+
+    const progressbar_canvas = document.getElementById("progressbar")
+    var progressbar_ctx = progressbar_canvas.getContext("2d")
+
+    function updateProgress(numerator, denominator)
+    {
+        let rect_y =0.05 * progressbar_canvas.height;
+        let rect_x = 0.05 * progressbar_canvas.width ;
+        let rect_width = 0.8 * progressbar_canvas.width ;
+        let rect_height = 0.5 * progressbar_canvas.height ;
+        progressbar_ctx.clearRect(0,0, progressbar_canvas.width, progressbar_canvas.height);
+        progressbar_ctx.rect(rect_x, rect_y, rect_width, rect_height);
+        progressbar_ctx.fillStyle='#1b7ced'
+        progressbar_ctx.fillRect(rect_x, rect_y, (numerator/denominator)*rect_width, rect_height)
+        progressbar_ctx.stroke();
+    }
+        function updateProgress_1(numerator, denominator)
+    {
+        progressbar_ctx.clearRect(0,0, progressbar_canvas.width, progressbar_canvas.height);
+        progressbar_ctx.beginPath();
+        progressbar_ctx.arc(50,50,40,0,2 * Math.PI * (numerator/denominator))
+        progressbar_ctx.stroke();
+    }
+
+
+    var hire_request_milestone = true
+    function generateRandomNumber(score, hire_request) {
+        let randomNumber = Math.ceil(Math.random() * hire_request) + score;
         return randomNumber;
     }
 
-    skill = 5
+    hire_request = 0
 
       // Set the initial number
     let count = 0;
 
       // Add an event listener to the button to increment the number
-    button.addEventListener("click", () => {
-        button.disabled = true; // Disable the button
-        count=generateRandomNumber(count, skill);
-        number.innerHTML = `Wood ${count}`;
+    paperwork_completing_button.addEventListener("click", () => {
+        paperwork_completing_button.disabled = true; // Disable the button
+        count= count+1 //generateRandomNumber(count, hire_request);
+        paperwork_value_p.innerHTML = `Completed paperwork ${count}`;
+        updateProgress(count-old_paperwork_milestone, paperwork_milestone-old_paperwork_milestone);
+        console.log(paperwork_milestone)
+        if (count == paperwork_milestone){
+                old_paperwork_milestone = paperwork_milestone;
+                paperwork_milestone=Math.ceil(2*paperwork_milestone)
+                
+            }
+
         setTimeout(() => {
-            button.disabled = false; // Re-enable the button after 1 second
+            paperwork_completing_button.disabled = false; // Re-enable the button after 1 second
         }, 1);
     });
 
-    skill_button.addEventListener("click", () => {
-        skill_button.disabled = true; // Disable the button
+    hire_request_button.addEventListener("click", () => {
+        hire_request_button.disabled = true; // Disable the button
         if (count > 30)
         {   
             count = count - 30
-            skill++
-            skill_number.innerHTML = `Skill ${skill}`;
-            number.innerHTML = `Wood ${count}`;
+            hire_request++
+            hire_request_number.innerHTML = `hire_request ${hire_request}`;
+            number.innerHTML = `Paperwork Completed ${count}`;
         } else
         {
             console.log("Not enough wood")
         }
 
 
-        if (skill_milestone && (skill > 15))
+        if (hire_request_milestone && (hire_request > 15))
         {
-            skill_milestone=false
-            alert("Skill bulk buy enabled")
-            skill_bulk.innerHTML ="<p>Click the button bulk buy wood skill:</p> \
-            <button id='skill-bulk-button'>Bulk increase skill</button>"
+            hire_request_milestone=false
+            alert("hire_request bulk buy enabled")
+            hire_request_bulk.innerHTML ="<p>Click the button bulk buy wood hire_request:</p> \
+            <button id='hire_request-bulk-button'>Bulk increase hire_request</button>"
 
-            const skill_bulk_button = document.getElementById('skill-bulk-button')
+            const hire_request_bulk_button = document.getElementById('hire_request-bulk-button')
 
-            skill_bulk_button.addEventListener("click", () => {
+            hire_request_bulk_button.addEventListener("click", () => {
                 button.disabled = true; // Disable the button
                 
                 while(count > 30)
                 {
                     count = count - 30;
-                    skill++;
+                    hire_request++;
                 }
-                skill_number.innerHTML = `Skill ${skill}`;
+                hire_request_number.innerHTML = `hire_request ${hire_request}`;
                 number.innerHTML = `Wood ${count}`;
 
                 setTimeout(() => {
@@ -70,6 +111,6 @@
 
 
         setTimeout(() => {
-            skill_button.disabled = false; // Re-enable the button after 1 second
+            hire_request_button.disabled = false; // Re-enable the button after 1 second
         }, 1);
     });
